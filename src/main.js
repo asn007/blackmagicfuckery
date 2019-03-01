794add9001d9e3386136e6749b49a4845e2ea520
Andrey Sinitsyn <andrey.sin98@gmail.com>
2019-03-01 12:15:41 +0300

    refactor: main.js coding style

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 async function render() {
-  const sum = await import(/* webpackChunkName: "sum" */ './sum')
-  const multiply = await import(/* webpackChunkName: "multiply" */ './multiply')
+  const sum = await import(
+    /* webpackChunkName: "sum.js" */ './sum'
+  )
+  const multiply = await import(
+    /* webpackChunkName: "multiply.js" */ './multiply'
+  )
   document.querySelector('#results').innerHTML = `
     <p>sum(2, 3): ${sum.default(2, 3)}</p>
     <p>multiply(2, 3): ${multiply.default(2, 3)}</p>
@@ -8,18 +12,21 @@ async function render() {
   const regex = /eval\("(.*)"\)/gm
   document.querySelector('#demo').value =
     regex
-      .exec(__webpack_require__.m['multiply.js'].toString())[1].replace(/\\n/g, '\n')
+      .exec(__webpack_require__.m['multiply.js'].toString())[1]
+      .replace(/\\n/g, '\n')
 }
 
 async function recompile(file = 'multiply.js', source) {
   delete __webpack_require__.c[file]
   window.webpackJsonp.push([
-    [file.split('.')[0]],
+    [file],
     {
-      [file]: eval(`(function(module, __webpack_exports__, __webpack_require__) {
-        "use strict";
-        eval("${source.replace(/\n/g, '\\n')}")
-      })`)
+      [file]: eval(
+        `(function(module, __webpack_exports__, __webpack_require__) {
+          "use strict";
+          eval("${source.replace(/\n/g, '\\n')}")
+        })`
+      )
     }
   ])
 }
